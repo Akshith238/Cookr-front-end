7e119720bb21e8c995c57d32b78a2dae2aff0910
Akshith238 <you@example.com>
2024-04-18 15:18:27 +0530

    next

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -20,7 +20,7 @@ const AboutSection = () => {
           </Typography>
           <Typography variant="body1" className="sm:text-start text-center sm:text-lg font-poppins text-xs opacity-80 font-medium text-wrap text-white mb-6">
             Chilly Flakes is a dynamic team of software systems students from PSG Tech, adept in the realms of Full Stack Web Development and Machine Learning. Our diverse skill set, honed through rigorous academic training and hands-on experience, equips us to tackle complex challenges with confidence.
-            As participants in the Cookr Food Tech Hackathon, we bring a fresh perspective and a commitment to innovation. Our passion for technology drives us to push boundaries and explore novel solutions that bridge the gap between culinary artistry and cutting-edge technology.
+            we bring a fresh perspective and a commitment to innovation. Our passion for technology drives us to push boundaries and explore novel solutions that bridge the gap between culinary artistry and cutting-edge technology.
             With a focus on crafting intuitive and user-centric web applications, coupled with our ability to leverage data-driven insights through machine learning, Chilly Flakes is poised to make a significant impact in the food tech industry. We're excited to collaborate, innovate, and showcase our talents as we embark on this exciting journey         
           </Typography>
         </motion.div>
--- src/components/project1/Project1Nav.js
+++ src/components/project1/Project1Nav.js
@@ -35,7 +35,6 @@ return (
              </div>
           </Toolbar>
       </AppBar>
-  
 </div>
 )
 }
